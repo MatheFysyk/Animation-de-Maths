c5df3703f5a63c00ab17bbcf75fcd3eb7372a985
Animfysyk <79930196+Animfysyk@users.noreply.github.com>
2022-04-13 21:48:23 +0200

    Create Ensembles de Julia et de Mandelbrot.py

---
+++ Ensembles de Julia et de Mandelbrot.py
@@ -0,0 +1,157 @@
+from manim import *
+from numba import njit
+
+
+array_of_complex_constants = [
+    [complex(np.pi/17, np.pi/17), MathTex('\\frac{\\pi}{17} + \\frac{\\pi}{17}i')],
+    [complex(np.pi/17, 42/1975), MathTex("\\frac{\\pi}{17} + \\frac{42}{1975}i")],
+    [complex(-1.975, 0.42), MathTex("-1.975 + 0.42i")],
+    [complex(np.pi/17, np.exp(-42)), MathTex("\\frac{\\pi}{17} + e^{-42}i")],
+    [complex(0.3, 0.03), MathTex("0.3 + 0.03i")],
+    [complex(0.42, 1), MathTex("0.42 + i")],
+    [complex(-1/15, 2/3), MathTex("-\\frac{1}{15} + \\frac{2}{3}i")],
+    [complex(-1.877, 0.115), MathTex("-1.877 + 0.115i")],
+    [complex(-1, 1), MathTex("-1 + i")],
+    [complex(0, 0), MathTex("0")],
+    [complex(1/42, np.pi/17), MathTex("\\frac{1}{42} + \\frac{\\pi}{17}i")],
+    [complex(-1.683, 0.6977), MathTex("-1.683 + 0.6977i")],
+    [complex(0.07, 0.7), MathTex("0.07 + 0.7i")]
+]
+
+
+def MandelbrotSet(max_steps: int = 50, resolution: int = 1080, x_range: np.ndarray = np.array([-2, 0.5]), y_range: np.ndarray = np.array([-1.15, 1.15])) -> np.ndarray:
+    x_m, x_M, y_m, y_M = x_range[0], x_range[1], y_range[0], y_range[1]
+    resolution_tuple = resolution, int(np.abs(x_M - x_m)/np.abs(y_M - y_m)*resolution)
+    dx, dy = np.abs(x_M - x_m)/resolution_tuple[1], np.abs(y_M - y_m)/resolution_tuple[0]
+    array = np.zeros(resolution_tuple, dtype='uint8')
+    for n in range(len(array)):
+        for p in range(len(array[0])):
+            c = complex(x_m + p*dx, y_M - n*dy)
+            z = c
+            i = 0
+            while i < max_steps and np.abs(z) <= 2:
+                z = z**2 + c
+                i += 1
+            if i == max_steps:
+                array[n, p] = 0
+            else:
+                array[n, p] = i*(255/max_steps)
+    return array
+
+
+
+def JuliatSet(complex_constant: complex = complex(0, 0), max_steps: int = 50, resolution: int = 1080, x_range: np.ndarray = np.array([-2, 0.5]), y_range: np.ndarray = np.array([-1.15, 1.15])) -> np.ndarray:
+    x_m, x_M, y_m, y_M = x_range[0], x_range[1], y_range[0], y_range[1]
+    resolution_tuple = resolution, int(np.abs(x_M - x_m)/np.abs(y_M - y_m)*resolution)
+    dx, dy = np.abs(x_M - x_m)/resolution_tuple[1], np.abs(y_M - y_m)/resolution_tuple[0]
+    array = np.zeros(resolution_tuple, dtype='uint8')
+    for n in range(len(array)):
+        for p in range(len(array[0])):
+            c = complex(x_m + p*dx, y_M - n*dy)
+            z = c
+            i = 0
+            while i < max_steps and np.abs(z) <= 2:
+                z = z**2 + complex_constant
+                i += 1
+            if i == max_steps:
+                array[n, p] = 0
+            else:
+                array[n, p] = i*(255/max_steps)
+    return array
+  
+  
+  
+  class MandelbrotAndJuliaScene(Scene):
+    def construct(self):
+        jited_MandelbrotSet = njit()(MandelbrotSet)
+        jited_JuliaSet = njit()(JuliatSet)
+        mandelbrot_set = ImageMobject(jited_MandelbrotSet(
+            max_steps=50,
+            resolution=1080,
+        )).set_z_index(4)
+        mandelbrot_set_complex_plane = ComplexPlane(
+            x_range = np.array([-2, 0.5]),
+            y_range = np.array([-1.15, 1.15]),
+            x_length = mandelbrot_set.height,
+            y_length = mandelbrot_set.width 
+        )
+        old_height = mandelbrot_set.height
+        mandelbrot_set.height = 3.5
+        rectangle = BackgroundRectangle(mandelbrot_set, color=WHITE, stroke_width=2, stroke_opacity=1, fill_opacity=0, buff=0).set_z_index(5)
+        Group(mandelbrot_set, rectangle).to_corner(UR, buff=0.2)
+
+        mandelbrot_set_complex_plane.move_to(mandelbrot_set).scale(3.5/old_height)
+
+        c = ComplexValueTracker(array_of_complex_constants[0][0])
+        max_steps = ValueTracker(20)
+
+        dot = always_redraw(lambda: Dot(mandelbrot_set_complex_plane.c2p(c.get_value().real, c.get_value().imag), radius=DEFAULT_SMALL_DOT_RADIUS, color=PURE_BLUE).set_z_index(6))
+        label = always_redraw(lambda: MathTex('c', stroke_color=BLACK, stroke_width=0.75).scale(1.25).next_to(dot, UL, buff=0.1).set_z_index(6))
+
+        eq = MathTex('z \\mapsto z^2 + c').to_corner(UL, buff=0.27).set_z_index(4)
+        rectangle_eq = SurroundingRectangle(eq, color=WHITE, buff=0.25).set_z_index(5)
+
+        constant = MathTex('c =').to_edge(buff=0.22).to_edge(DOWN, buff=0.65).set_z_index(10)
+        for complex_constant in array_of_complex_constants:
+            complex_constant[1].next_to(constant, RIGHT)
+        constant_value = array_of_complex_constants[0][1].set_z_index(10)
+        background = always_redraw(lambda: BackgroundRectangle(Group(constant, constant_value), color=BLACK))
+        surrounding = always_redraw(lambda: SurroundingRectangle(Group(constant, constant_value), color=WHITE, buff=0.2)).set_z_index(11)
+
+        julia_set = ImageMobject(jited_JuliaSet(
+            c.get_value(),
+            max_steps=int(max_steps.get_value()),
+            resolution=1440,
+            x_range=np.array([-1.75, 1.75]),
+            y_range=np.array([-1.5, 1.5])
+        )).set_z_index(3)
+        julia_set.height = config.frame_height
+
+        self.play(FadeIn(Group(mandelbrot_set, rectangle, eq, rectangle_eq, julia_set, dot, label, constant, constant_value, background, surrounding)))
+        self.wait(1.6)
+
+        def update_julia_set(julia_set):
+            julia_set.become(ImageMobject(jited_JuliaSet(
+                c.get_value(),
+                max_steps=int(max_steps.get_value()),
+                resolution=1440,
+                x_range=np.array([-1.75, 1.75]),
+                y_range=np.array([-1.5, 1.5])
+            )).set_z_index(3))
+            julia_set.height = config.frame_height
+
+        julia_set.add_updater(update_julia_set)
+
+        for k in range(1, len(array_of_complex_constants)):
+            if k == 4:
+                self.play(
+                    c.animate.set_value(array_of_complex_constants[k][0]),
+                    max_steps.animate.set_value(35),
+                    Transform(array_of_complex_constants[0][1], array_of_complex_constants[k][1]),
+                    run_time=3
+                    )
+                self.wait(1.6)
+            if k == 6:
+                self.play(
+                    c.animate.set_value(array_of_complex_constants[k][0]),
+                    max_steps.animate.set_value(500),
+                    Transform(array_of_complex_constants[0][1], array_of_complex_constants[k][1]),
+                    run_time=3
+                    )
+                self.wait(1.6)
+            if k == 12:
+                self.play(
+                    c.animate.set_value(array_of_complex_constants[k][0]),
+                    max_steps.animate.set_value(70),
+                    Transform(array_of_complex_constants[0][1], array_of_complex_constants[k][1]),
+                    run_time=3
+                    )
+                self.wait(1.6)
+            if k == 1 or k == 2 or k == 3 or k == 5 or k == 7 or k == 8 or k == 9 or k == 10 or k==11:
+                self.play(
+                    c.animate.set_value(array_of_complex_constants[k][0]),
+                    max_steps.animate.set_value(20),
+                    Transform(array_of_complex_constants[0][1], array_of_complex_constants[k][1]),
+                    run_time=3
+                    )
+                self.wait(1.6)
